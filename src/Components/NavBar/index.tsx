178bbe978002a57682d911f19b3a8f2d1fe1bce3
Thalita Leandra de Oliveira Costa <t.de.oliveira.costa@avanade.com>
2023-09-29 14:00:27 -0300

    fix: remove import

--- src/Components/NavBar/index.tsx
+++ src/Components/NavBar/index.tsx
@@ -1,7 +1,6 @@
 import "./styles.scss"
 import SearchIcon from '@mui/icons-material/Search';
 import DarkModeIcon from '@mui/icons-material/DarkMode';
-import LanguageIcon from '@mui/icons-material/Language';
 import ListIcon from '@mui/icons-material/List';
 import { useContext } from "react";
 import { DarkModeContext } from "@/Context/darkModeContext";
